8a58da5ce9e1af25c4813edd460cd9e424c918fa
Sergey Makletsov <smaklets@kpfu.ru>
2023-11-23 13:39:37 +0300

    Добавьте файлы проекта.

---
+++ L2k-2023-11-23-Graphics2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L2k_2023_11_23_Graphics2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private List<MyFigure> fList = new List<MyFigure>();
+        private MyFigure currentFigure;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            fList.Add(new MyFigure());
+            MainCanvas.Children.Add(fList.Last().FigureShape);
+        }
+
+        private Point? startPoint = null;
+        private void MainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition((IInputElement)sender);
+            if (startPoint is { } point) {
+                fList.ForEach(
+                    f => { if (f.ContainsPoint(point)) currentFigure = f; }
+                );
+            }
+        }
+
+        private void MainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = null;
+        }
+
+        private void MainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (startPoint is { } point)
+            {
+                if (currentFigure.ContainsPoint(point))
+                {
+                    var currentPoint = e.GetPosition((IInputElement)sender);
+                    currentFigure.Move(point, currentPoint);
+                    startPoint = currentPoint;
+                }
+            }
+        }
+    }
+}
---
+++ L2k-2023-11-23-Graphics2/MyFigure.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace L2k_2023_11_23_Graphics2
+{
+    public class MyFigure
+    {
+        public Shape FigureShape { get; set; }
+        public double Width => FigureShape.Width;
+        public double Height => FigureShape.Height;
+        public double X => FigureShape.Margin.Left;
+        public double Y => FigureShape.Margin.Top;
+        public MyFigure() {
+            FigureShape = new Ellipse();
+            FigureShape.Width = 100;
+            FigureShape.Height = 100;
+            FigureShape.Fill = Brushes.Green;
+            FigureShape.Stroke = Brushes.Blue;
+            FigureShape.StrokeThickness = 4;
+        }
+
+        public void Move(Point from, Point to)
+        {
+            FigureShape.Margin = new Thickness(
+                FigureShape.Margin.Left + to.X - from.X,
+                FigureShape.Margin.Top + to.Y - from.Y,
+                FigureShape.Margin.Right,
+                FigureShape.Margin.Bottom
+            );
+        }
+
+        public bool ContainsPoint( Point point )
+        {
+            return point.X >= X
+                && point.X <= X + Width
+                && point.Y >= Y
+                && point.Y <= Y + Height;
+        }
+    }
+}
